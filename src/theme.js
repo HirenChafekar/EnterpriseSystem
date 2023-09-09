9355f30814dd9ad8589979d32917a051857591f0
HirenChafekar <hirenchafekar1@gmail.com>
2023-09-09 19:30:03 +0530

    settings up topbar

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,7 @@ import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
 
 // color design tokens
-export const tokens = (mode = {
+export const tokens = (mode) => ({
   ...(mode === "dark"
     ? {
         grey: {
